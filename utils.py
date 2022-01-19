1f80f8576806cd294d37776d7eeafbda5a09f9fa
magnusbarata <barata.magnuz@gmail.com>
2022-01-19 20:57:00 +0900

    Utils implementation: - build_dataset(): return class map - set_seed() - limit_gpu_memory()

--- utils.py
+++ utils.py
@@ -1,7 +1,37 @@
 import os
+import re
 import json
 import shutil
 import pandas as pd
+
+def set_seed(seed):
+    """Function setting the global seed for reproducible results"""
+    import tensorflow as tf
+    import numpy as np
+    import random
+
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+
+
+def limit_gpu_memory(mem_limit, gpu_idx=0):
+    """Function to limit GPU memory to be allocated by tf"""
+    import tensorflow as tf
+
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            tf.config.set_logical_device_configuration(
+                gpus[gpu_idx],
+                [tf.config.LogicalDeviceConfiguration(memory_limit=mem_limit)]
+            )
+        except RuntimeError as e:
+            # Virtual devices must be set before GPUs have been initialized
+            print(e)
+
 
 def continue_train_on(path):
     try:
@@ -17,6 +47,7 @@ def continue_train_on(path):
             else:
                 print('Exiting...')
                 raise SystemExit
+
 
 class Params:
     """Class to load/save hyperparameters from/to a json file.
@@ -44,12 +75,17 @@ class Params:
     def dict(self):
         return self.__dict__
 
+
 def build_dataset(data_dir, fout='dataset.csv'):
     dirpath, _, fnames = next(os.walk(data_dir))
     df = {'Fpath': fnames, 'Label': []}
+    cls_map = {'nor': 0, 'pd': 1} # Basic case is binary classification
     for i, fname in enumerate(fnames):
-        label = 1 if fname[:2] == 'PD' else 0
+        label_str = re.match(r'\D+', fname)[0].lower()
+        if cls_map.get(label_str) is None:
+            cls_map[label_str] = len(cls_map)
         fnames[i] = f'{dirpath}/{fname}'
-        df['Label'].append(label)
+        df['Label'].append(cls_map[label_str])
     
     pd.DataFrame(df).to_csv(fout, index=False)
+    return cls_map
